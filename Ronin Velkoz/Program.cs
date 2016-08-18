d3ab14c0be9b29bce79596186cda726fbf529bd7
AkumaCORE <k-sintu@hotmail.com>
2016-08-18 14:38:06 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -272,18 +272,18 @@ namespace RoninVelkoz
 	                        var endPos = Handle.Position.To2D() + 1100 * perpendicular;
 	
 	                        var collisionObjects = EntityManager.Heroes.Enemies.Where(it => it.IsValidTarget(1500));
-	                        if (collisionObjects.Count() >= 1)
-	                        {
-	                           Chat.Print("enemy");
+	                        //if (collisionObjects.Count() >= 1)
+	                       // {
+	                           
 	                            
-	                        }
+	                        //}
 	                        
 	                        foreach (var hero in collisionObjects)
 	                        {
 		                if ( Prediction.Position.Collision.LinearMissileCollision(hero, startPos, endPos, 2000, 100, 0))
 	                    	{
 	
-	
+				   Chat.Print("enemy");
 	                           SpellsManager.Q.Cast(Champion);
 	
 	                           

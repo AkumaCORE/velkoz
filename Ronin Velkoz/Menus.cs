f4fc4c74f77091029eea097dbde56bd89555312d
AkumaCORE <k-sintu@hotmail.com>
2016-08-18 14:18:44 -0400

    Update Menus.cs

--- Ronin Velkoz/Menus.cs
+++ Ronin Velkoz/Menus.cs
@@ -27,7 +27,7 @@ namespace RoninVelkoz
         public const string LastHitMenuID = "lasthitmenuid";
         public const string JungleClearMenuID = "jungleclearmenuid";
         public const string KillStealMenuID = "killstealmenuid";
-        public const string DrawingsMenuID = "drawingsmenuid";
+       // public const string DrawingsMenuID = "drawingsmenuid";
         public const string MiscMenuID = "miscmenuid";
         public static Menu FirstMenu;
         public static Menu ComboMenu;
@@ -37,7 +37,7 @@ namespace RoninVelkoz
         public static Menu LasthitMenu;
         public static Menu JungleClearMenu;
         public static Menu KillStealMenu;
-        public static Menu DrawingsMenu;
+        //public static Menu DrawingsMenu;
         public static Menu MiscMenu;
 
         //These colorslider are from Mario`s Lib
@@ -62,7 +62,7 @@ namespace RoninVelkoz
             JungleClearMenu = FirstMenu.AddSubMenu("• JungleClear", JungleClearMenuID);
             KillStealMenu = FirstMenu.AddSubMenu("• KillSteal", KillStealMenuID);
             MiscMenu = FirstMenu.AddSubMenu("• Misc", MiscMenuID);
-            DrawingsMenu = FirstMenu.AddSubMenu("• Drawings", DrawingsMenuID);
+            //DrawingsMenu = FirstMenu.AddSubMenu("• Drawings", DrawingsMenuID);
 
             ComboMenu.AddGroupLabel("Combo");
             ComboMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
@@ -158,22 +158,22 @@ namespace RoninVelkoz
             MiscMenu.CreateComboBox("3rd Spell to focus", "thirdFocus", new List<string> {"Q", "W", "E"}, 2);
             MiscMenu.CreateSlider("Delay slider", "delaySlider", 200, 150, 500);
 
-            DrawingsMenu.AddGroupLabel("Settings");
-            DrawingsMenu.CreateCheckBox(" - Draw Spell`s range only if they are ready.", "readyDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw damage indicator.", "damageDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw damage indicator percent.", "perDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw damage indicator statistics.", "statDraw", false);
-            DrawingsMenu.AddGroupLabel("Spells");
-            DrawingsMenu.CreateCheckBox(" - Draw Q.", "qDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw W.", "wDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw E.", "eDraw");
-            DrawingsMenu.CreateCheckBox(" - Draw R.", "rDraw");
-            DrawingsMenu.AddGroupLabel("Drawings Color");
-            QColorSlide = new ColorSlide(DrawingsMenu, "qColor", Color.Red, "Q Color:");
-            WColorSlide = new ColorSlide(DrawingsMenu, "wColor", Color.Purple, "W Color:");
-            EColorSlide = new ColorSlide(DrawingsMenu, "eColor", Color.Orange, "E Color:");
-            RColorSlide = new ColorSlide(DrawingsMenu, "rColor", Color.DeepPink, "R Color:");
-            DamageIndicatorColorSlide = new ColorSlide(DrawingsMenu, "healthColor", Color.YellowGreen, "DamageIndicator Color:");
+           // DrawingsMenu.AddGroupLabel("Settings");
+            //DrawingsMenu.CreateCheckBox(" - Draw Spell`s range only if they are ready.", "readyDraw");
+           // DrawingsMenu.CreateCheckBox(" - Draw damage indicator.", "damageDraw");
+            //DrawingsMenu.CreateCheckBox(" - Draw damage indicator percent.", "perDraw");
+            //DrawingsMenu.CreateCheckBox(" - Draw damage indicator statistics.", "statDraw", false);
+           // DrawingsMenu.AddGroupLabel("Spells");
+//DrawingsMenu.CreateCheckBox(" - Draw Q.", "qDraw");
+            //DrawingsMenu.CreateCheckBox(" - Draw W.", "wDraw");
+            //DrawingsMenu.CreateCheckBox(" - Draw E.", "eDraw");
+          //  DrawingsMenu.CreateCheckBox(" - Draw R.", "rDraw");
+           // DrawingsMenu.AddGroupLabel("Drawings Color");
+          //  QColorSlide = new ColorSlide(DrawingsMenu, "qColor", Color.Red, "Q Color:");
+           // WColorSlide = new ColorSlide(DrawingsMenu, "wColor", Color.Purple, "W Color:");
+          //  EColorSlide = new ColorSlide(DrawingsMenu, "eColor", Color.Orange, "E Color:");
+           // RColorSlide = new ColorSlide(DrawingsMenu, "rColor", Color.DeepPink, "R Color:");
+          //  DamageIndicatorColorSlide = new ColorSlide(DrawingsMenu, "healthColor", Color.YellowGreen, "DamageIndicator Color:");
         }
         public static int HitchanceQ
         {
